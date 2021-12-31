92d04ff1490f11931d5fade2f655801e23087401
AvikRao <avik.rao@gmail.com>
2021-12-31 17:57:25 -0500

    Switch to using MNIST dataset

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,1 @@
-import os 
-
-ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-cwd = os.getcwd()
-basis_dir = os.path.join(cwd, "basis_images")
-
-for letter in ALPHABET :
-    letter_dir = os.path.join(basis_dir, letter)
-    os.chdir(letter_dir)
-    files = os.listdir()
-    for i in range(len(files)) :
-        os.system(f"convert -resize 400x400\! {files[i]} -compress none {str(i).zfill(3)}.ppm")
-        # os.remove()
+import os
